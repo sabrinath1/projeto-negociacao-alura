92be42aa8d43db7ec7e72d28838bebf9341047e3
Sabrina <sabrinath1@hotmail.com>
2022-06-27 16:14:28 -0300

    code reduction changes part 2

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -20,4 +20,11 @@ export class Negociacao {
     get volume() {
         return this._quantidade * this._valor;
     }
+    static criaDe(dataString, quantidadeString, valorString) {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
 }
